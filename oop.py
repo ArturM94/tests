911d498eec535c21b593a615e89509ad4e6a6bcc
Artur Manukian <manukian.artur.94@gmail.com>
2019-04-13 13:54:07 +0300

    Create Runner class

--- oop.py
+++ oop.py
@@ -5,3 +5,15 @@ class Person:
 
     def walk(self):
         return f'{self.name} is walking'
+
+
+class Runner(Person):
+    def __init__(self, name, age, speed):
+        super().__init__(name, age)  # inherits 'name' and 'age' attributes from parent class Person
+        self.speed = speed
+
+    def walk(self):  # redefined method
+        return f'{self.name} is walking to stadium'
+
+    def run(self):  # own unique method
+        return f'{self.name} is running with speed {self.speed} km/h'
