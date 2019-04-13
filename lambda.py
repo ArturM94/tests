6627b53ad4f0d41ac722b6cc45f7fe1ccf82f1dc
Artur Manukian <manukian.artur.94@gmail.com>
2019-04-13 13:06:08 +0300

    Create sequence using lambda function in list comprehension

--- lambda.py
+++ lambda.py
@@ -4,3 +4,6 @@ indexes = range(10)
 
 lambda_mapped_array = list(map(lambda x: 0 if x % 2 == 0 else 1, indexes))
 print(lambda_mapped_array)
+
+lambda_list_comprehension_array = [(lambda index: 0 if index % 2 == 0 else 1)(index) for index in indexes]
+print(lambda_list_comprehension_array)
