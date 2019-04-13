600a05abc1d85111b3993a7a96dda856ec818f08
Artur Manukian <manukian.artur.94@gmail.com>
2019-04-13 14:06:31 +0300

    Create Driver class

--- oop.py
+++ oop.py
@@ -19,6 +19,15 @@ class Runner(Person):
         return f'{self.name} is running with speed {self.speed} km/h'
 
 
+class Driver(Person):
+    def __init__(self, name, age, driver_license):
+        super().__init__(name, age)
+        self.driver_license = driver_license
+
+    def drive(self):  # own unique method
+        return f'{self.name} is driving with driver license №{self.driver_license}'
+
+
 person = Person('Person', 30)
 print(person.walk())
 
